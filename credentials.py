61fbedac4f7c60e941e1a7e73a88108071ea3f85
Raphael Menezes de Jesus <61888147+RaphaelJesus1@users.noreply.github.com>
2023-05-01 22:17:51 -0300

    service access credentials

---
+++ credentials.py
@@ -0,0 +1,12 @@
+credentials = {
+    "type": "service_account",
+    "project_id": "robust-builder-382122",
+    "private_key_id": "719b503156a894572f2291bdadf9991ebb68c2fc",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC5oG4K/NliigGl\nZzw8WJQfa8eTcTBx9uzmd1nMPiN1nW1VSs9PTd7tdQ0v660VIadB22UZWsm7bslK\nN7YMKln7+Jbw7xZ4TcrbxpjQdmKw/5Zi8lt7eVBNz458HKjNtPRkYSiyeH9WIuQM\nLNcQEvE0r7bpir+x7pcJ0RylGCc7r27j6YLgC8xSOrEj1bAP+7e8+emaD+NLaX3S\nf7dwX8eiIiHTOf2NN0T7C1nXhlAoEIQQXTLZrULYBcsMByVUyRy6UKgqWpGBsWBO\nOBSpgrP2YczjX7t3m7j1QFPnn0QDeXFihi/hU0QUleUtb9aE+Uyv8jIFdP61rWqq\ntWa4oAjjAgMBAAECggEAFdORXOViQj3r96Dt6kKYGCG6M9IJWj0omejKvENKNBgw\ntOi4+6AyaEVtKYdSsLtmSU4c0yBjvBBIxttKIqAQOJqAVRAn2jl7cuAlM83XyOwk\nqLnKm/glwyB2po1QI+/1FKCAAow8nMo5el0DZLZ1ejZ3EXrpE2j0hYj7D0aJxnCk\n16/0zjH9vG50TyMUmXbQYHGdwPpZFxQMw4TQvIMjOvsRvm1t9Bi4nJPI8u8o7bO3\n1xIYfgwh3tf9DOT4O6GLntf55h4GpCoXxCbYDuDMcJr6qRFQdxXlBi0yv1SqdM8A\nT3OKg4oOybuCjmwR5TXXLPIzucWgugNB3RApFd2bkQKBgQDxUHmdtaOJiua4Sfof\n+GcA3TOFKGte1kFZZASaGDKukl4CpKRAkDuYp3+Vm5z9EFtKQ9tv5hnFOUrplMQz\nQ4Q1oWNOgu0sbwLnpMcEDsBcd08pJThL3Xw//QUD7xpv6oW1QDlkVkzCA4xX/ZUT\n/JskjntwmI9ZV06sFYGEHNOQDwKBgQDE7GBD1xAYTu3kmY/1/uJS1mgr4dv+draq\nQnGr4mEQWpHNJKVn5GCTGg+JRrzhLs7jMvZQmbnlkaVKahr6BUAkzw6t5iab8pDX\nM0m5cTRg4mwpOQFyYRqeX72S7S1D77W1JH9bwI738O+hMCp6u6tqadbw3ZqM3RM0\nTpMGWnal7QKBgG52mDVJYIMlwle6BXV9ECmlGGBsEDppeaCh2DlxukGPXtl25Hlp\njN5kTlL92O8+/YsRZPHkMwfFIjWqk+WjknG6Xusho9AjQQEovLFuzz+EnggHsa2I\nxcfE2vbiDQgOIwbFT4BOwPRPC3975TIxABPL5VW1gGk1Ph4n+hGv4GihAoGAE0ja\nvIoJJ3hDT8nCEWMeouIgXXvG4YqapN3Q5/ZJbyKPeLmIAT6aPo2ky9rDkIHW/OT2\nvjnvvI2g1ELD1YxCFdi5JUqOqWL90DVTmPd/G6XQ1dChVaQwySWzGPplHmwRLWYf\nmsqmtZ1AulZPMqnCRa+LvxbUlcrooIy5VTvytpkCgYAY6kd4He9OQCoxNk0EyWjN\nR14sJhW2/zaSWA60hABI2j1iGa3mYRv1L3U8IerK0e0Zl8ZaW+Qza2u9AXwYCikx\nnq6lkaRfIO7CStLVzVg68MdIklPoKpN6KcWnCzRvQDY73aCk/wC6qhcxqScNUriS\nwoL+i/zJvyLMNLeOiq63qA==\n-----END PRIVATE KEY-----\n",
+    "client_email": "bot-dataops@robust-builder-382122.iam.gserviceaccount.com",
+    "client_id": "111742669586277989730",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/bot-dataops%40robust-builder-382122.iam.gserviceaccount.com"
+}
